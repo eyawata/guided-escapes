ef9f1cdf54754d4ccff46bc9009dd0b6a07fffab
Eri Yawata <yawata.eri@gmail.com>
2024-08-13 15:17:19 +0900

    Bookings create method

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,5 +3,21 @@ class BookingsController < ApplicationController
   end
 
   def create
+    @booking = Booking.new()
+    @user = current_user
+    @experience = Experience.find(params[:id])
+    @booking.user = @user
+    @booking.experience = @experience
+    if @booking.save
+      redirect to experience_path(experience)
+    else
+      render "#", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:date)
   end
 end
