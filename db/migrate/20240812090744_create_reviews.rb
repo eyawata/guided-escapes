5951ea4a8cc6099763cbd6c051e3c3c2d44a89df
Eri Yawata <yawata.eri@gmail.com>
2024-08-13 12:07:19 +0900

    Edit Schema

--- db/migrate/20240812090744_create_reviews.rb
+++ db/migrate/20240812090744_create_reviews.rb
@@ -1,10 +1,10 @@
 class CreateReviews < ActiveRecord::Migration[7.1]
   def change
     create_table :reviews do |t|
-      t.references :experiences, foreign_key: true
+      t.references :experience, foreign_key: true
       t.text :content
       t.float :rating
-      t.references :users, foreign_key: true
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
