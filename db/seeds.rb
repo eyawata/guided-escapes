0298f683c792528feb7e8a630f8155a666d58845
sadanan vorlaruxtara <sadanan.vorlaruxtara@gmail.com>
2024-08-13 15:12:35 +0900

    added booking.destroy_all to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,7 @@ require 'faker'
 # db/seeds.rb
 
 # Clear existing data
+Booking.destroy_all
 Experience.destroy_all
 User.destroy_all
 
@@ -47,19 +48,18 @@ experiences = [
 # create two users - guide and traveller
 
 10.times do
-  user = User.create!(
-  first_name: Faker::Name.first_name,
-  last_name: Faker::Name.last_name,
-  email: Faker::Internet.email,
-  password: '123123' # needs to be at least 6 characters
- )
+  User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: '123123' # needs to be at least 6 characters
+  )
 end
-
 
 puts "Creating experience!"
 
 experiences.each do |experience|
-  exp = Experience.create!(
+  Experience.create!(
     location: experience[:location],
     title: experience[:title],
     content: experience[:content],
@@ -68,5 +68,6 @@ experiences.each do |experience|
     duration: rand(2..10),
     user: User.all.sample
   )
-  end
-  puts "Seed data created !"
+end
+
+puts "Seed data created!"
