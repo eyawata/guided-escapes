76248bd53610de9413b3ecafaa51e4451ef1930b
ali haddi <alij2297@hotmail.com>
2024-08-13 18:09:53 +0900

    Add: Index view

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -2,6 +2,7 @@ class ExperiencesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
+    @experiences = Experience.all
   end
 
   def show
