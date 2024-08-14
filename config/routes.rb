ede5f05aeaea31b76c5945bef7c7169ec63327ca
eyawata <157731956+eyawata@users.noreply.github.com>
2024-08-14 13:44:15 +0900

    Revert "Added landing page"

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "experiences#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :experiences, only: [:index, :show] do
