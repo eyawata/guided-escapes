73d17918fe4b4f6d254f951e19687d73d52a7a83
Eri Yawata <yawata.eri@gmail.com>
2024-08-15 17:18:03 +0900

    Added user profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@ class User < ApplicationRecord
   has_many :bookings
   has_many :reviews
   has_many :bookings_as_guide, through: :experiences, source: :bookings
+
+  has_one_attached :avatar
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: [:show]
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
