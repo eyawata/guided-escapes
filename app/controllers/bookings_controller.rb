6c47970aaaa1d492fa3755b39ba6fa5c17236bc9
Jonas Al-Taher <jonas.taher@hotmail.com>
2024-08-15 14:28:11 +0900

    checkpoint for creating experiences workflow

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -14,7 +14,6 @@ class BookingsController < ApplicationController
     if @booking.save
       redirect_to bookings_path
     else
-      raise
       render "#", status: :unprocessable_entity
     end
   end
