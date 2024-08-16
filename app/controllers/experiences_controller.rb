bf80547de861c48c039214b6cdb4c2258a33eab5
sadanan vorlaruxtara <sadanan.vorlaruxtara@gmail.com>
2024-08-16 13:27:47 +0900

    Completed popup but moving to master to pull other updates

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -6,7 +6,8 @@ class ExperiencesController < ApplicationController
     @markers = @experiences.geocoded.map do |experience|
       {
         lat: experience.latitude,
-        lng: experience.longitude
+        lng: experience.longitude,
+        info_window_html: render_to_string(partial: "info_window", locals: { experience: experience })
       }
     end
   end
